7afb7b36efa1d594806aef44702aee04c52e64ca
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-01-17 23:47:37 +0100

    Finished apps basics

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // Bring `HashMap` into scope
 use std::collections::HashMap;
 
-fn main() {
+fn main() -> () {
     // `mut` to opt-in to mutation
     // `.skip()` to skip the first arg (the location of the package)
     let mut args = std::env::args().skip(1);
@@ -12,9 +12,10 @@ fn main() {
 
     let db = Database::new();
 
-    let contents = format!("{}\t{}\n", key, value);
+    let t = db.ok().unwrap().to_string();
+    let contents = format!("{}{}\t{}\n", t, key, value);
 
-    std::fs::write("kv.db", contents);
+    std::fs::write("kv.db", contents).expect("Error writing to file");
 
     // Exclamation signifies the func is a macro
     println!("Key: {} and Value: {}", key, value);
@@ -33,17 +34,37 @@ impl Database {
 
         // Same as above ^
         let contents = std::fs::read_to_string("kv.db")?;
+        let mut inner = HashMap::new();
 
         for line in contents.lines() {
             let chunks: Vec<&str> = line.split("\t").collect();
+            if chunks.len() != 2 {
+                todo!("Return error")
+            }
+
+            let key = chunks[0];
+            let value = chunks[1];
+
+            inner.insert(key.to_owned(), value.to_owned());
 
             // Debugger
-            dbg!(chunks);
+            // dbg!(chunks);
         };
+
         Ok(
             Database {
-                inner: std::collections::HashMap::new()
+                inner
             }
         )
+    }
+
+    fn to_string(&self) -> String {
+        let mut final_string = String::from("");
+
+        for k in self.inner.iter() {
+            final_string = format!("{}{}\t{}\n", final_string, k.0, k.1);
+        }
+
+        return final_string;
     }
 }
